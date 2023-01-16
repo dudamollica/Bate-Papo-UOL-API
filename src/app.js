89c190d3e744efb10b251019e9f6e43d87b1d2e0
dudamollica <dudarenno@yahoo.com.br>
2023-01-16 15:22:24 -0300

    Tratamento de erros

--- src/app.js
+++ src/app.js
@@ -39,11 +39,12 @@ server.get("/participants", (req, res) => {
 server.post("/participants", async (req, res) => {
   const { name } = req.body;
   user = req.body.name;
+  const participants = req.body
 
-  const nameSchema = joi.object({
+  const participantsSchema = joi.object({
     name: joi.string()
   })
-  const validation = nameSchema.validate(name)
+  const validation = participantsSchema.validate(participants)
   if (validation.error) return res.sendStatus(422)
   
   const userExist = await db.collection("participants").findOne({ name });
@@ -66,6 +67,17 @@ server.post("/participants", async (req, res) => {
 
 server.get("/messages", (req, res) => {
   const limit = parseInt(req.query.limit)
+  
+    const verifyLimit = req.query
+
+    const limitSchema = joi.object({
+      limit: joi.number().greater(0)
+    })
+
+    const validation = limitSchema.validate(verifyLimit)
+    if (validation.error) return res.sendStatus(422) 
+
+
   db.collection("messages")
     .find()
     .toArray()
@@ -90,6 +102,17 @@ server.get("/messages", (req, res) => {
 
 server.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
+  const messages = req.body
+  const messagesSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required()
+  })
+  const validation = messagesSchema.validate(messages)
+  if (validation.error) return res.sendStatus(422)
+
+  // if(!req.headers.user) return res.sendStatus(422)
+
   try {
     await db.collection("messages").insertOne({
       from: req.headers.user,
